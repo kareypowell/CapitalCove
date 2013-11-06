c37251a3db131aad090b89d06a1b7b80f7c6d0a4
lawrencegrey <lawrencegrey.0125@gmail.com>
2013-11-06 15:11:53 -0800

    Create CapitalCoveEdge.php

---
+++ CapitalCoveEdge.php
@@ -0,0 +1,109 @@
+
+<?php
+ $page_title = 'Capital Cove - The Capital Cove Edge';
+ include 'includes/pagehead.php';
+?>
+
+<div class="site-body">
+
+	<div class="pe-spacer size70"></div>
+
+	<div class="pe-container">
+		
+		<div class="row-fluid">
+			
+			<!--main content-->
+			<section class="span8">
+				
+				<!--new post-->
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="post single">
+							<!--post titles-->
+							<div class="row-fluid">
+								<div class="span12 post-title">
+									<h1><a href="#">The Capital Cove Edge</a></h1>
+								</div>
+							</div>
+							<!--meta-->
+							
+													
+							<div class="row-fluid">
+								<div class="span12">
+									<div class="span12">
+										<p class="intro">
+										  <p>
+                       <strong> A Firm That is Clearly Built for These Times. </strong>
+
+It's a new financial and real estate world. In an economy and regulatory environment that is shifting and changing faster than ever before, active investors who want real answers that unlock the best foreclosure and distressed real estate investment opportunities, and successfully manage ever growing risk, entrust their confidence, capital and assets to Capital Cove. As an independent, REO Financial Services and Investment Advisor, our firm has no greater responsibility to our individual and institutional investors. Given the current economic climate, regulatory environment concerning foreclosure relief services, excessive real estate-owned (REO) inventory and the lack of liquidity in the Capital Markets – we invite you to consider the following five points to consider when investing in the "New World": 
+                      </p>
+
+                      
+										  <blockquote>
+                        <p>
+                          <ul class="blockquote_list">
+                            <li>
+                            Seek stronger diversification across asset classes that are actually making money, and are well positioned for future upside. Distressed real estate, non-performing mortgage portfolio assets and alternative investments are now the "New" investments for active and entrepreneurial investors.
+                            </li>
+                            <li>
+                             Realize the benefits of Alternative Investments, such as precious metals, long/short strategies, distressed real estate, defaulted mortgages/deeds of trust, private placements, preferred stock and a wide variety of insurance products and private equity vehicles.
+                            </li>
+                            <li>
+                             Focus on higher yield, asset-based investments that grow in value over time. Everyone has seen the extreme danger to wealth management in "securitization of paper" and hedging strategies based on nothing but overhyped speculation.
+                            </li>
+                            <li>
+                             Be proactive about your financial future and building a top performing portfolio for the long term. Through REO Fixed income plans, Variable Annuities, Separately Managed Accounts and Self-Directed IRA Plans – everyone should be vested in their financial future.
+                            </li>
+                            <li>
+                              Only work with reputable, knowledgeable real estate and investment firms that have a verifiable track record in foreclosures, REO real estate and alternative investments. In a distressed real estate world weighed down by "daisy chain intermediaries" and foreclosure rescue scams – some people still think that it doesn't matter who you do business with…until it does.
+                            </li>
+                           
+                          </ul>
+                        </p>
+                      </blockquote>
+
+                   
+								
+
+                    <br>
+        	  <p>
+                    Capital Cove has built its core offerings around its clients' greatest financial needs for capital preservation, transparency, equity participation and investment opportunities that truly generate wealth:	 providing specialized skill sets and a depth of transactional capabilities across active and passive investment strategies. Our solutions-based and client focus on delivering strong, consistent and profitable performance across asset classes, geographies and investment strategies is a cornerstone of our competitive advantage. It is why owners, asset managers, government sponsored entities (GSEs) equity stakeholders and investors partner with Capital Cove to help them build a more dynamic, risk-managed and diverse portfolio that these trying economic times require. 
+                    </p>
+
+                   
+                      <strong>
+                        Please  contact us at:  877.639.6120 or email us at:   inquiries@capitalcove.com.
+                      </strong>
+                   
+                    
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+				<!--end post-->
+				
+				
+				
+
+					
+			</section>
+
+			<!--sidebar-->
+				<?php REQUIRE 'includes/sidebar.php';?>
+			<!--end sidebar-->		
+
+		</div><!--end row 1-->
+		
+		
+		
+	</div>
+</div>
+
+
+	
+ 	<div class="pe-spacer size100"></div>
+	 	<?php REQUIRE 'includes/footer.php';?>
