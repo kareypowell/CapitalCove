f812257df0a76f5d118668f374086f4a48298bd0
lawrencegrey <lawrencegrey.0125@gmail.com>
2013-11-06 19:43:16 -0800

    Create OurTrackRecord.php

---
+++ OurTrackRecord.php
@@ -0,0 +1,112 @@
+
+<?php
+ $page_title = 'Capital Cove - Our Track Record';
+ include 'includes/pagehead.php';
+?>
+
+
+	<div class="pe-spacer size70"></div>
+
+	<div class="pe-container">
+		
+		<div class="row-fluid">
+			
+			<!--main content-->
+			<section class="span8">
+            
+            
+				
+				<!--new post-->
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="post single">
+							<!--post titles-->
+							<div class="row-fluid">
+								<div class="span12 post-title">
+									<h1><a href="#">Our Track Record</a></h1>
+								</div>
+							</div>
+							<!--meta-->
+							
+							<!--post image-->
+							<div class="row-fluid">
+								<div class="span12  post-image">
+									<a href="#"><img src="img/content/TrackRecord.png" class="peLazyLoading" data-original="img/content/TrackRecord.png" width="620" height="349" alt="alt text" /></a>
+								</div>
+							</div>
+							
+							<div class="row-fluid">
+								<div class="span12">
+									<div class="span12">
+										<p class="intro">
+										  <blockquote>
+                        <p>
+                       
+                  PROVEN AND VERIFIABLE TRACK RECORD ACROSS ALL REO ASSET CLASSES
+                        </p>
+                      </blockquote>
+
+							
+                            	<!--post image-->
+							<div class="row-fluid">
+								<div class="span12  post-image">
+									<a href="#"><img src="img/content/Track-Record-Boxes.png" class="peLazyLoading" data-original="img/content/Track-Record-Boxes.png" width="620" height="349" alt="alt text" /></a>
+								</div>
+							</div>	
+                            	
+                        <p>
+                         
+                      We Fit Solutions to Clients.  Not the Other Way Around.
+                      
+                      
+  <blockquote>
+                      <p>
+                        <ul class="blockquote_list">
+                          <li>Cash </li>
+                           <li> Cash Equivalents</li>
+                            <li> Stocks</li>
+                             <li> Real Property </li>
+                              <li> Qualified IRA Plans</li>
+                               <li> In-kind Exchanges</li>
+                                <li> Structured Equity Transactions</li>
+                                 
+                        </ul>
+                        10% due on commitment.  Call period - 5 business days.
+                        </p>
+                        </blockquote>
+                        </p>
+ 
+                       
+
+                 
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+				<!--end post-->
+				
+				
+				
+
+					
+			</section>
+
+			<!--sidebar-->
+				<?php REQUIRE 'includes/sidebar.php';?>
+			<!--end sidebar-->		
+
+		</div><!--end row 1-->
+		
+		
+		
+	</div>
+</div>
+
+
+	
+ 	<div class="pe-spacer size100"></div>
+	 	<?php REQUIRE 'includes/footer.php';?>
