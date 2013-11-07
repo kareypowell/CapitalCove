f8cf9dff69e5dc0a030984f49c7b624cf459067b
lawrencegrey <lawrencegrey.0125@gmail.com>
2013-11-06 19:10:04 -0800

    Create CorporateCitizenship.php

---
+++ CorporateCitizenship.php
@@ -0,0 +1,120 @@
+
+<?php
+ $page_title = 'Capital Cove - Corporate Citzenship';
+ include 'includes/pagehead.php';
+?>
+
+
+	<div class="pe-spacer size70"></div>
+
+	<div class="pe-container">
+		
+		<div class="row-fluid">
+			
+			<!--main content-->
+			<section class="span8">
+				
+				<!--new post-->
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="post single">
+							<!--post titles-->
+							<div class="row-fluid">
+								<div class="span12 post-title">
+									<h1><a href="#">Corporate Citizen</a></h1>
+								</div>
+							</div>
+							<!--meta-->
+							
+						
+							
+							<div class="row-fluid">
+								<div class="span12">
+									<div class="span12">
+										<p class="intro">
+										  <blockquote>
+                        <p>
+                       
+                  
+                        </p>
+                      </blockquote>
+
+									
+                        <p>
+                         
+                      Through its Donor-Advised Fund, the Global Zakat Charitable Foundation, Capital Cove is dedicated to a broad range of real estate, infrastructure and financial services that improve the lives and financial well-being of the communities in which we operate. 
+                        </p>
+ 
+                         <p>
+                         
+                   Capital Cove is an established Leader in Donor-Advised Fund Investment and development-focused initiatives that offer investors, government entities, and non-profit organizations solutions that deliver maximum flexibility, targeted development and tangible impact. We believe and work diligently to provide a framework that promotes sound investment objectives and the development needs of society at large. 
+
+                        </p>
+                   
+							
+                             
+                         <p>
+                        The Capital Cove Entrepreneurship and Development Initiative<br /><br />
+
+Through the Global Zakat Charitable Foundation and several leading Donor-Advised Funds, Capital Cove Financial is committed to supporting the communities where we work and live. We always seek to apply our real estate and investment expertise to contribute to the success of a range of investment vehicles, qualified projects, and organizations that have demonstrated an innovative approach to improving the infrastructure, educational and economic conditions of underserved communities. Several key sectors in which we seek to invest, develop or finance include:
+
+
+  <blockquote>
+                      <p>
+                        <ul class="blockquote_list">
+                          <li>Unfinished Residential and Commercial Developments </li>
+                           <li> Real Estate-Owned Single Family or Multi-Family Properties</li>
+                            <li> Entitled Land in Urban Areas for New Housing Developments</li>
+                             <li> Low to Moderate Income Housing Initiatives</li>
+                              <li> Transitional Living and/or Housing Shelters</li>
+                               <li> Co-Investments or Bridge Capital in qualified rehab or substantial construction projects</li>
+                                <li> Mixed-use Office Buildings</li>
+                                 <li> Pre-Foreclosure Single Family properties and/or portfolios</li>
+                        </ul>
+                        
+                        </p>
+                        </blockquote>
+                                              </p>
+                                              
+                                           <p>
+                                           Working directly with individuals, institutions, advisors and lenders, we have developed a highly tactical approach to charitable giving as it relates to strategic use of time, real estate assets, money and resources. We work hard to develop and execute intelligent, useful and ethical assistance, delivered in a manner that works best for all parties. 
+                                           </p>
+                 
+
+                                           <p>
+                                          In addition to its primary focus on real estate, entrepreneurship and alternative investments, the Foundation makes numerous grants, monetary credits and co-investments to support charitable projects. Moreover, the firm's employees and partners give generously of their time in an effort to deliver a bigger impact on civic, real estate and charitable organizations that support real estate and infrastructure development in the communities where Capital Cove's employees, clients and investors live and work. 
+
+Please contact us at:  <strong> 909-457-8440 or email us at:  inquiries@capitalcove.com. </strong>
+                                           </p>
+                 
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+				<!--end post-->
+				
+				
+				
+
+					
+			</section>
+
+			<!--sidebar-->
+				<?php REQUIRE 'includes/sidebar.php';?>
+			<!--end sidebar-->		
+
+		</div><!--end row 1-->
+		
+		
+		
+	</div>
+</div>
+
+
+	
+ 	<div class="pe-spacer size100"></div>
+	 	<?php REQUIRE 'includes/footer.php';?>
