652be5c41631f1077542ad2fadab0f3b0ce0aab0
lawrencegrey <lawrencegrey.0125@gmail.com>
2013-11-06 18:49:08 -0800

    Create PeopleAndCulture.php

---
+++ PeopleAndCulture.php
@@ -0,0 +1,107 @@
+
+<?php
+ $page_title = 'Capital Cove - People & Culture';
+ include 'includes/pagehead.php';
+?>
+
+
+	<div class="pe-spacer size70"></div>
+
+	<div class="pe-container">
+		
+		<div class="row-fluid">
+			
+			<!--main content-->
+			<section class="span8">
+				
+				<!--new post-->
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="post single">
+							<!--post titles-->
+							<div class="row-fluid">
+								<div class="span12 post-title">
+									<h1><a href="#">People & Culture</a></h1>
+								</div>
+							</div>
+							<!--meta-->
+							
+							<!--post image-->
+							<div class="row-fluid">
+								<div class="span12  post-image">
+									<a href="#"><img src="img/content/PeopleAndCulture.png" class="peLazyLoading" data-original="img/content/PeopleAndCulture.png" width="620" height="349" alt="alt text" /></a>
+								</div>
+							</div>
+							
+							<div class="row-fluid">
+								<div class="span12">
+									<div class="span12">
+										<p class="intro">
+										  <blockquote>
+                        <p>
+                         PEOPLE, WORKPLACE ENVIRONMENT & CORPORATE CULTURE 
+                  
+                        </p>
+                      </blockquote>
+
+									
+                        <p>
+                          <h3>Joining The Best Minds And Managers In The Business</h3><br>
+                         We look for professional candidates, executives and entrepreneurs with a strong interest or background in global capital markets, portfolio management, real estate services, alternative asset management, loss mitigation/default asset management or similar financial services background. We also consider individuals that are likely to thrive in our corporate culture, since a key to Capital Cove's continued success is how well we work together, deliver for our clients and exceed industry benchmarks set forth by the best and top performing firms in our industry.
+Since inception, Capital Cove has effectively aligned its interests with those of its Investor and Principal Owner clients, and other professionals who have seen their successes drive the firm's success. As evidence of that alignment, since 2006 the firm and its wholly owned subsidiaries have completed hundreds of REO investment, real estate and foreclosure engagements – closing transactions across each and every recognized REO asset class in the industry.
+                        </p>
+ 
+                         <p>
+                          <h3>Reaping The Financial Benefits And Career Rewards That You Have Earned</h3><br>
+                        When you join Capital Cove, you will work with state of the art technology from day one, and benefit from our established business model, which offers you experience in real estate markets and REO portfolio assets nationwide. What's more, Capital Cove offers you highly competitive pay incentives, multiple streams of income generation, comprehensive and ongoing training, flexible work options and many professional development opportunities.
+                        </p>
+                   
+							
+                             
+                         <p>
+                          <h3>Supporting Diversity & Inclusion In The Communities We Serve</h3><br>
+                        At Capital Cove, we strongly believe diversity enriches and strengthens the culture dynamic of our firm and allows us to better serve our clients. By leveraging our respective differences and valuing diversity of thought, backgrounds, experiences, and national origins – we are able to better serve our investor and principal owner clients, while maintaining our competitive advantages as an employer of choice and industry leader in the REO financial services, investment advisory and asset management arenas.
+
+                        </p>
+                        
+                              <p>
+                          <h3>Helping Property Owners, Lenders, Investors And Equity Stakeholders Thrive</h3><br>
+                       Capital Cove has always been invested in helping our individual and institutional clients thrive -- whether by repositioning distressed real estate assets, investing alongside qualified investors, helping to improve the infrastructure of the communities in which we operate, consistently delivering double-digit annualized returns to our investor base, or simply supporting the dedication and interests of our clients and employees. Our business units and team members represent the passion, energy and entrepreneurial spirit that grew us from a small foreclosure services and real estate company in 2005 to the national firm we are today. Building a career with us goes beyond single real estate transactions and traditional investments. We pride ourselves on making a difference in the financial lives of those who entrust us with their capital, portfolio assets, retirement and investment goals – clients and partners who count on us to help them secure their financial future.<br /><br />
+We invite you to experience Capital Cove's formula for success, which combines an entrepreneurial spirit and true REO financial services acumen with an unmatched depth of market knowledge and state-of-the-art technology to successfully compete in the ultra-competitive REO Real Estate and distressed assets marketplace
+
+                        </p>
+                   			
+
+                 
+
+									</div>
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div>
+				<!--end post-->
+				
+				
+				
+
+					
+			</section>
+
+			<!--sidebar-->
+				<?php REQUIRE 'includes/sidebar.php';?>
+			<!--end sidebar-->		
+
+		</div><!--end row 1-->
+		
+		
+		
+	</div>
+</div>
+
+
+	
+ 	<div class="pe-spacer size100"></div>
+	 	<?php REQUIRE 'includes/footer.php';?>
