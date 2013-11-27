9073eea67365e36b6a2bd4932bb128b57735b2ea
lawrencegrey <lawrencegrey.0125@gmail.com>
2013-11-27 08:33:21 -0800

    changes to various files

--- OffMarketIntermediariesAndLimitedPartners.php
+++ OffMarketIntermediariesAndLimitedPartners.php
@@ -39,12 +39,88 @@
 								<div class="span12">
 									<div class="span12">
 										<p class="intro">
+                                     <strong>    CAPITAL COVE'S </strong>
 										  <blockquote>
+                                           PRIORITY REFERRAL PARTNER (PRP) NETWORK <br />
+    REAL VALUE-ADD RELATIONSHIPS, OFF-MARKET OPPORTUNITIES<br />
+    AND REFERRAL RELATIONSHIPS THAT CLOSE BUSINESS.<br />
+ 
+                                          </blockquote> </p>
                         <p>
+                        
                        
-              Page Under Construction...
+       <strong>   Proven investment and foreclosure expertise on your side of the transaction. </strong> <br />
+At Capital Cove, we believe the continued strong and consistent growth of our businesses can accelerate quicker with the trusted association of real estate entrepreneurs, contracted fiduciaries, and our global network of professional investors, advisors and affiliated partner firms. Our long-term relationships with registered intermediaries and referral consultants create a true sense of working with one another, in a non-competitive and principal-direct manner, to help achieve successful outcomes for the buyer, seller or investor of a real estate-owned asset. If you are the titled owner or lien holder of an off-market, for sale by owner (FSBO), or principal-direct transaction, we would like to hear from you. You may also register your opportunity on this website, email your opportunity to reo@capitalcove.com, or request a New Client Registration form from our office staff.
+                        </p>
+                     
+<p> 
+Services that are tailor fit for contracted intermediaries and off-market referral sources.
+</p> 
+
+<p>
+At Capital Cove, we apply our proven experience, competence and capital to help ensure a tailored fit for your unique REO real estate or foreclosure-based opportunity.
+</p> 
+
+<p>
+We can easily help you with:
+</p>
+
+<p> 
+<blockquote>
+
+  <ul class="blockquote_list">
+  	<li>Putting an opportunity into escrow immediately with full capital allocated to close. </li> 
+    <li> Opening escrow immediately and providing a turnkey, seamless transaction from start to close.</li> 
+     <li> Efficient and effective implementation of investment strategy or repositioning.</li> 
+      <li>  Quick valuation and term sheet offered prepared within 24 hours of receipt of a referral.</li> 
+       <li> Public and private offer management system set up for the benefit of the title asset owner. </li> 
+        <li>Adherence to strict "anti-daisy chain" protocols. </li> 
+        
+         <li>Fully transparent offer, escrow, payment and closing procedures. </li> 
+  </ul>
+	
+</blockquote>
+
+</p>
+
+<p>
+Trusted referral sources and contracted intermediaries are important to our firm's continued investment and asset management success. Our firm knows how to close business, and we know how to share the benefits of a successful closing with the people who make it happen. At Capital Cove, we partner and contract with all types of intermediaries including:
+</p>
+
+  <div style="margin-left:0;margin-right:25px;" class="span6">
+                      <blockquote>
+                        <h3></h3>
+                        <p>
+                          <ul class="blockquote_list">
+                            <li>Attorneys and Law Firms</li>
+                            <li>Real Estate Entrepreneurs</li>
+                            <li>CPAs and Accounting Firms</li>
+                            <li>Investment Advisors and Managers</li>
+                            <li>Appraisers and Court Receivers  </li>
+                          
+                          
+                          </ul>
                         </p>
                       </blockquote>
+                    </div>
+
+                    <div class="span5">
+                      <blockquote>
+                        <h3></h3>
+                        <p>
+                          <ul class="blockquote_list">
+                            <li>Business Sale Brokers</li>
+                            <li>Private Lenders and Entities</li>
+                            <li>Rehabbers, Builders and Developers</li>
+                            <li>FSBO Property Owners</li>
+                            <li>Loan Sale Advisors and Note Sellers</li>
+                           
+                          </ul>
+                        </p>
+                      </blockquote>
+                    </div>
+
+
 
 							
                             	<!--post image-->
--- PropertyOwnersAndEquityStakeholders.php
+++ PropertyOwnersAndEquityStakeholders.php
@@ -42,7 +42,9 @@
 										  <blockquote>
                         <p>
                        
-              Page Under Construction...
+               MULTI-ASSET REO REAL ESTATE SOLUTIONS<br />
+   DESIGNED WITH YOUR REAL ESTATE<br />
+   GOALS IN MIND.<br />
                         </p>
                       </blockquote>
 
@@ -55,12 +57,51 @@
 							</div>	
                             	
                         <p>
-                         
+                         Let's focus on the real goals and disposition strategies that solve your problem. <BR /> <Br />
+ 
+At Capital Cove, we stand with real property owners and equity stakeholders as true business partners in the strongest sense of the term. Whether you are a private property owner, investor, private lender, or an entity guided by an advisor's personalized advice, we believe our value-added services provide the best way to reach your desired outcomes. Our strong experience with all property types and default scenarios, and verifiable closings in every REO real estate asset class, allows us to deliver immediate capital allocation, streamlined acquisition to disposition solutions, and turnkey REO asset management solutions to real property owners nationwide – either with or without the assistance of traditional real estate agents or brokers.
                      
+                      </p>
                       
+                      <p>
+                   <strong>   Why should property owners and equity stakeholders care about multi-asset REO solutions? </strong><br /><br />
+ 
+The real estate marketplace has changed dramatically - and now includes the explosive growth of new industry dynamics such as the entry of institutional Wall Street firms, offers from all-cash international buyers, increased off-market/for sale by owner transactions, and new internet realty firms and tech savvy service companies that are able to close real estate transactions quicker, better and less expensively than traditional brokerage firms.
+                      </p>
 
  
-                       
+                      <p>
+                      If you are a goal-oriented and active property owner or investor, then we think you should consider multi-asset REO real estate solutions. As the name implies, Capital Cove looks at your real estate asset or portfolio from multiple angles, and devises the optimal acquisition, management or disposition solution for you or your organization. In addition to superior standard real estate services and REO asset management solutions, Capital Cove offers property owners and equity stakeholders:
+                      </p> 
+                      
+                 
+                      
+                      <p> 
+<blockquote>
+
+  <ul class="blockquote_list">
+  	<li>Residential and Commercial Sales, Brokerage and Investment Solutions </li>
+    
+    <li>Real-Time Valuation and a Two-Tiered Disposition Model</li>
+    
+        <li> Loan Sales, Workout or Restructuring</li>
+        
+            <li> Quick Refinancing or Recapitalization on Defaulted Real Estate Assets </li>
+                <li>Off-Market and Principal-Direct Only Transactions  that deliver speed to market </li>
+                    <li>Asset Enhancement and then Sale to maximize resale value </li>
+                        <li>Profitable Equity Joint Venture Partnerships that create win-win closings </li>
+    
+    </ul> </blockquote>
+                      
+                      </p>
+                      
+                      <p>
+                      Proven Experience and Comprehensive Resources
+                      </p>
+                      
+                      <p>
+                      Capital Cove's core operating groups are comprised of seasoned and knowledgeable professionals from across our Real Estate Business Platform. Our local real estate expertise, combined with our recognized track record in real estate capital markets, enables our teams to resolve complex real estate problems quickly, and execute optimal strategies on a timely basis.
+                      </p>
 
                  
 									</div>
