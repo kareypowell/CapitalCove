782bfed400aa78ea66635bae895635fdfea5e4ae
Karey Powell <kareyp9@gmail.com>
2013-11-27 14:36:16 -0500

    Modify layout for WhatWeDo page

--- WhatWeDo.php
+++ WhatWeDo.php
@@ -5,133 +5,148 @@
 ?>
 
 
-	<div class="pe-spacer size70"></div>
+<div class="pe-spacer size70"></div>
 
-	<div class="pe-container">
-		
-		<div class="row-fluid">
-			
-			<!--main content-->
-			<section class="span8">
-            
-            
-				
-				<!--new post-->
-				<div class="row-fluid">
-					<div class="span12">
-						<div class="post single">
-							<!--post titles-->
-							<div class="row-fluid">
-								<div class="span12 post-title">
-									<h1><a href="#">What We Do</a></h1>
-								</div>
-							</div>
-							<!--meta-->
-							
-							<!--post image-->
-							<div class="row-fluid">
-								<div class="span12  post-image">
-									<a href="#"></a>
-								</div>
-							</div>
-							
-							<div class="row-fluid">
-								<div class="span12">
-									<div class="span12">
-										<p class="intro">
-										  <blockquote>
-                        <p>
-                       CORPORATE OVERVIEW <BR /><BR />
-					   CAPITAL COVE COMMITS PEOPLE,<BR />
-					   OUR CLIENTS, INVENTORS, SHAREHOLDERS<BR />
-					   AND PARTNERS TO: 
-					   
-	                        </p>
-                      </blockquote>
+<div class="pe-container">
+  <div class="row-fluid">
+    <!--main content-->
 
-							
-                            	<!--post image-->
-							<div class="row-fluid">
-								<div class="span12  post-image">
-									
-								</div>
-							</div>	
-                            	
-                        <p>
-                         <b>I</b>NVEST &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;in high-yield alternative investments that grow their wealth<br />
-                         <br />
- 
-<b>D</b>IVERSIFY&nbsp;&nbsp;their portfolios with REO and distressed mortgage portfolio assets <br />
-<br />
- 
-<b>P</b>ROFIT&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;from the market displacement in the current real estate and mortgage marketplace and the unprecedented opportunities it presents <br /><br />
- 
-Capital Cove is a client-focused and solutions-based real estate services firm focused
-primarily on real estate-owned (REO) acquisitions, investment advisory services,
-asset management, private equity real estate funds and defaulted mortgage
-portfolio assets. Our integrated services delivery model combines world-class
-investment and asset management expertise with exceptional client service skills,
-in-depth consultation, and an impressive track record of successfully closing
-complex real estate transactions. Since inception, we have excelled at sourcing,
-acquiring, managing and disposing of distressed REO real estate and mortgage
-portfolio assets on behalf of its own corporate portfolio, lenders, investors, affiliated
-partner firms, and institutional investment entities.
-                     
-                      </p>
+    <section class="span8">
+      <!--new post-->
 
- 
-                       
-<p>
-At Capital Cove, we: <br />
+      <div class="row-fluid">
+        <div class="span12">
+          <div class="post single">
+            <!--post titles-->
 
-<table align="center">
-<tr> <td width="221">Transact and Close:  </td> 
-<td width="645"> Real estate buyers, sellers, asset managers and investors count on us to provide exceptional brokerage, asset management, investment advisory and transaction services that deliver tangible value.</td> 
-</tr>
-<tr> <td>Advise and Execute: </td> <td> We advise individual and institutional investors on buying, selling, investing or developing distressed REO real estate assets, and showing them how to invest in foreclosure assets with confidence.
-</td> </tr>
-<tr> <td>Innovate and Lead: </td> <td> To date, our firm has developed proprietary investment programs and acquisition strategies that protect capital, generate double-digit returns, and are tailor-made for our clients.</td> </tr>
-<tr> <td>Manage and Perform: </td> <td> We exist to do great things and add value by preserving wealth, growing assets, and exceeding the expectations of our individual and institutional clients.</td> </tr>
-<tr> <td>Partner and Finance: </td> <td>When and where applicable, we finance deals and invest alongside our registered clients, global investor base and affiliate partners firms aligning our mutual interests at all times.</td> </tr>
+            <div class="row-fluid">
+              <div class="span12 post-title">
+                <h1><a href="#">What We Do</a></h1>
+              </div>
+            </div><!--meta-->
+            <!--post image-->
 
-</table>
-</p>
+            <div class="row-fluid">
+              <div class="span12 post-image">
+                <a href="#"><img alt="" src="http://placehold.it/620x349" class="peLazyLoading" data-original="http://placehold.it/620x349" width="620" height="349" ></a>
+              </div>
+            </div>
 
-<p> 
-Between today's foreclosure marketplace and tomorrow's investment exit
-strategies, unprecedented wealth building opportunities can be found. To see how
-our professionals can deliver customized real estate sales, investment, brokerage
-and asset management solutions to you or your organization, please contact us at.
-</p>
-                 
-									</div>
-								</div>
-							</div>
-							
-						</div>
-						
-					</div>
-				</div>
-				<!--end post-->
-				
-				
-				
+            <h3>CORPORATE OVERVIEW</h3>
 
-					
-			</section>
+            <div class="row-fluid">
+              <div class="span12">
+                <div class="span12">
+                  <blockquote>
+                    <p>
+                    	<span style="margin-left:-15px">
+                    	CAPITAL COVE COMMITS PEOPLE,<br>
+                    	</span>
+                    	<span style="margin-left:-15px">
+                    	CAPITAL COVE EXPERTISE TO HELP <br>
+	                    </span>
+	                    <span style="margin-left:-15px">
+	                    OUR CLIENTS, INVENTORS, SHAREHOLDERS<br>
+		                  </span>
+		                  <span style="margin-left:-15px">
+	                    AND PARTNERS TO:
+		                  </span>
 
-			<!--sidebar-->
-				<?php REQUIRE 'includes/sidebar.php';?>
-			<!--end sidebar-->		
 
-		</div><!--end row 1-->
-		
-		
-		
-	</div>
+	                    <ul class="blockquote_list">
+												<li><b>INVEST</b> in high-yield alternative investments that grow their wealth.</li>
+												<li><b>DIVERSIFY</b> their portfolios with REO and distressed mortgage portfolio assets.</li>
+												<li>
+													<b>PROFIT</b> from the market displacement in the current real estate and mortgage
+													marketplace and the unprecedented opportunities it presents.
+                  			</li>
+	                    </ul>
+	                  </p>
+                  </blockquote>
+
+                  <p>
+	                  Capital Cove is a client-focused and solutions-based real
+	                  estate services firm focused primarily on real estate-owned
+	                  (REO) acquisitions, investment advisory services, asset
+	                  management, private equity real estate funds and defaulted
+	                  mortgage portfolio assets. Our integrated services delivery
+	                  model combines world-class investment and asset management
+	                  expertise with exceptional client service skills, in-depth
+	                  consultation, and an impressive track record of successfully
+	                  closing complex real estate transactions. Since inception, we
+	                  have excelled at sourcing, acquiring, managing and disposing
+	                  of distressed REO real estate and mortgage portfolio assets
+	                  on behalf of its own corporate portfolio, lenders, investors,
+	                  affiliated partner firms, and institutional investment
+	                  entities.
+	                </p>
+
+                  <h3>At Capital Cove, We:</h3>
+
+                  <table align="center">
+                    <tr>
+                      <td width="221"><b>Transact and Close:</b></td>
+
+                      <td width="645">Real estate buyers, sellers, asset
+                      managers and investors count on us to provide exceptional
+                      brokerage, asset management, investment advisory and
+                      transaction services that deliver tangible value.</td>
+                    </tr>
+										<tr><td width="221">&nbsp;</td><td width="645">&nbsp;</td></tr>
+                    <tr>
+                      <td><b>Advise and Execute:</b></td>
+
+                      <td>We advise individual and institutional investors on
+                      buying, selling, investing or developing distressed REO
+                      real estate assets, and showing them how to invest in
+                      foreclosure assets with confidence.</td>
+                    </tr>
+										<tr><td width="221">&nbsp;</td><td width="645">&nbsp;</td></tr>
+                    <tr>
+                      <td><b>Innovate and Lead:</b></td>
+
+                      <td>To date, our firm has developed proprietary
+                      investment programs and acquisition strategies that
+                      protect capital, generate double-digit returns, and are
+                      tailor-made for our clients.</td>
+                    </tr>
+										<tr><td width="221">&nbsp;</td><td width="645">&nbsp;</td></tr>
+                    <tr>
+                      <td><b>Manage and Perform:</b></td>
+
+                      <td>We exist to do great things and add value by
+                      preserving wealth, growing assets, and exceeding the
+                      expectations of our individual and institutional
+                      clients.</td>
+                    </tr>
+										<tr><td width="221">&nbsp;</td><td width="645">&nbsp;</td></tr>
+                    <tr>
+                      <td><b>Partner and Finance:</td>
+
+                      <td>When and where applicable, we finance deals and
+                      invest alongside our registered clients, global investor
+                      base and affiliate partners firms aligning our mutual
+                      interests at all times.</td>
+                    </tr>
+                  </table>
+									<br>
+                  <p>Between today's foreclosure marketplace and tomorrow's
+                  investment exit strategies, unprecedented wealth building
+                  opportunities can be found. To see how our professionals can
+                  deliver customized real estate sales, investment, brokerage
+                  and asset management solutions to you or your organization,
+                  please contact us at.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!--end post-->
+    </section><!--sidebar-->
+    <?php REQUIRE 'includes/sidebar.php';?><!--end sidebar-->
+  </div><!--end row 1-->
 </div>
 
+<div class="pe-spacer size100"></div>
 
-	
- 	<div class="pe-spacer size100"></div>
-	 	<?php REQUIRE 'includes/footer.php';?>
+<?php REQUIRE 'includes/footer.php';?>
